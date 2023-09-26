24e5272e1b9f46f0a6ea98a6a66899f79085ccf2
Juliana Rodrigues Nogueira <114073784+rodrigues28ju@users.noreply.github.com>
2023-09-26 12:11:37 -0300

    Create Aula1809.java

---
+++ Aula1809.java
@@ -0,0 +1,17 @@
+public class JavaApplication1 {    
+    public static void main(String[] args) {       
+        System.out.println("Primeira aula - Mulheres Tech");        
+        int idade;  //int tipo de dados inteiro.
+        String nome;//String tipo de dados alfanumérico.        
+        idade = 53;
+        nome = "Fatima Brito";        
+        System.out.println("Nome = "+nome+" Idade = "+idade);        
+        int valor1,valor2,soma;        
+        valor1 = 150;
+        valor2 = 200;
+        soma = valor1 + valor2;        
+        System.out.println("Soma de 150 e 200 = "+soma);            
+       
+    }
+   
+}
